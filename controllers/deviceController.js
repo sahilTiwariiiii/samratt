672bde296cd70302ea2e1120c24761906aecf3a6
sahilTiwariiiii <135219783+sahilTiwariiiii@users.noreply.github.com>
2025-08-28 13:11:51 +0530

    Update deviceController.js

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,4 +1,4 @@
-const DeviceReport = require("../models/DeviceReport")
+const DeviceReport = require("../models/deviceReport")
 const { validationResult } = require("express-validator")
 
 // Create new device report
@@ -169,4 +169,3 @@ module.exports = {
   updateDeviceReport,
   deleteDeviceReport,
 }
-
